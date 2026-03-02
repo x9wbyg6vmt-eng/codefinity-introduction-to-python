f5a458d607b7c5a36b6d7272d207b4ea8f91037c
x9wbyg6vmt-eng <>
2026-03-02 22:06:02 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,9 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+bakery = grocery_items[12:17]
+fruit1 = grocery_items[18:24]
+fruit2 = grocery_items[25:32]
+meat = grocery_items[33:40]
+
+print("We have dairy, bakery, fruit and meat items:", dairy1+", "+dairy2+", "+bakery+", "+fruit1+", "+fruit2+" and "+meat, "in asle 5.")
