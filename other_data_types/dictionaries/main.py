3cc0a0b8bfb2d4ee1302ae70a8090522aa9d62d9
x9wbyg6vmt-eng <>
2026-03-10 19:24:54 +0000

    Dictionaries

---
+++ other_data_types/dictionaries/main.py
@@ -0,0 +1,11 @@
+grocery_inventory = {
+    "Milk": (113, "Dairy"),
+    "Eggs": (116, "Dairy"),
+    "Bread": (117, "Bakery"),
+    "Apples": (141, "Produce")}
+bread_details = grocery_inventory["Bread"]
+print("Details of Bread:", bread_details)
+grocery_inventory.update({"Cookies": (143, "Bakery")})
+print("Inventory after adding Cookies:", grocery_inventory)
+grocery_inventory.pop("Eggs")
+print("Inventory after removing Eggs:", grocery_inventory)
